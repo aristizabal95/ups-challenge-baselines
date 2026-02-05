9a893e57e8f1b0d701cecd08bcc0e0992405a9cf
Alejandro Aristizabal <alejandro.aristizabal24@gmail.com>
2026-02-05 17:17:12 -0500

    Disable hardcoded tar downloading for all languages

--- ups_challenge/dataloaders/urls.py
+++ ups_challenge/dataloaders/urls.py
@@ -1,7 +1,6 @@
 import os
 import pickle
 
-import braceexpand
 from tqdm import tqdm
 
 from .build_index import build_lid_index
@@ -27,34 +26,26 @@ def build_urls(
     if hf_token is None:
         raise ValueError("HF_TOKEN is not set")
     token = f"Authorization:Bearer {hf_token}"
-    if len(langs) > 0:
-        if not os.path.exists(index_path):
-            build_lid_index(index_path, hf_token=hf_token)
+    if not os.path.exists(index_path):
+        build_lid_index(index_path, hf_token=hf_token)
 
-        with open(index_path, "rb") as f:
-            lid_index = pickle.load(f)
+    with open(index_path, "rb") as f:
+        lid_index = pickle.load(f)
 
-        all_relevant_tar_numbers = set()
-        for (tar_number, _), lang in tqdm(lid_index.items()):
-            if lang in langs:
-                all_relevant_tar_numbers.add(tar_number)
-        all_relevant_tar_numbers = list(all_relevant_tar_numbers)
-        urls = []
-        for tar_number in all_relevant_tar_numbers:
-            if int(tar_number) <= 5000:
-                urls.append(
-                    f"https://huggingface.co/datasets/MLCommons/unsupervised_peoples_speech/resolve/main/audio/{tar_number}.tar?download=True"
-                )
-            else:
-                urls.append(
-                    f"https://huggingface.co/datasets/MLCommons/unsupervised_peoples_speech/resolve/main/audio2/{tar_number}.tar?download=True"
-                )
-        urls = [f"pipe:curl -s -L {url} -H {token}" for url in urls]
-        return urls
-
-    else:
-        # Choose the number of tars to download
-        url = "https://huggingface.co/datasets/MLCommons/unsupervised_peoples_speech/resolve/main/audio/{000001..000004}.tar?download=True"
-        urls = list(braceexpand.braceexpand(url))
-        urls = [f"pipe:curl -s -L {url} -H {token}" for url in urls]
-        return urls
+    all_relevant_tar_numbers = set()
+    for (tar_number, _), lang in tqdm(lid_index.items()):
+        if len(langs) == 0 or lang in langs:
+            all_relevant_tar_numbers.add(tar_number)
+    all_relevant_tar_numbers = list(all_relevant_tar_numbers)
+    urls = []
+    for tar_number in all_relevant_tar_numbers:
+        if int(tar_number) <= 5000:
+            urls.append(
+                f"https://huggingface.co/datasets/MLCommons/unsupervised_peoples_speech/resolve/main/audio/{tar_number}.tar?download=True"
+            )
+        else:
+            urls.append(
+                f"https://huggingface.co/datasets/MLCommons/unsupervised_peoples_speech/resolve/main/audio2/{tar_number}.tar?download=True"
+            )
+    urls = [f"pipe:curl -s -L {url} -H {token}" for url in urls]
+    return urls
